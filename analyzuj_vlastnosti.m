f9d0cc06e1eb6eb75dcd075537ba2412968068be
Alexey Morozov <xmorozov@stuba.sk>
2019-12-17 10:39:30 +0100

    minor[analyzuj_vlastnosti]: rydzost added

--- analyzuj_vlastnosti.m
+++ analyzuj_vlastnosti.m
@@ -1,6 +1,10 @@
 function x=analyzuj_vlastnosti(cit,men)
 	if length(cit)<=length(men)
-       x=('System je fyzikalne realizovatelny.');
+        if length(cit)==length(men)
+            x=('System je rydzi.');
+        else 
+            x=('System je striktne rydzi.');
+        end
     else
         x=('System nie je fyzikalne realizovatelny!');
     end
