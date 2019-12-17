89d844f31359391fb280e603aad8895ae7c1fba0
xdzurkova <xdzurkova@stuba.sk>
2019-12-17 10:30:14 +0100

    [minor]: new m-files

---
+++ analyzuj_vlastnosti.m
@@ -0,0 +1,10 @@
+function x=analyzuj_vlastnosti(cit,men)
+	if length(cit)<=length(men)
+       x=('System je fyzikalne realizovatelny.');
+    else
+        x=('System nie je fyzikalne realizovatelny!');
+    end
+end
+
+
+
---
+++ testuj.m
@@ -0,0 +1,6 @@
+cit1=[1,2,3];
+men1=[3,4];
+cit2=[1,2];
+men2=[3,4,5];
+x1=analyzuj_vlastnosti(cit1,men1)
+x2=analyzuj_vlastnosti(cit2,men2)
